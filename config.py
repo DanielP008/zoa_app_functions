3cf29e3505cacdd10d6ef61d34e83847d1222a20
DanielP008 <danplsr.contact@gmail.com>
2026-01-30 16:49:37 +0100

    Update API token in config.py to use production credentials, ensuring secure access to the live environment.

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # Configuración global de la API
 API_BASE = "https://dev.api.zoasuite.com/api"
-TOKEN = "sk_test_9f8a7b6c5d4e3f2a"
+#TOKEN = "sk_test_9f8a7b6c5d4e3f2a"
+TOKEN = "prod_sk_7QfA9R2eWcLZp6mN4H8XxKJtD5yB0uS1VGaEOrU"
