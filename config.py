98e559bd8ebfda8a408892c58096cfb3a5a161d7
DanielP008 <danplsr.contact@gmail.com>
2026-02-03 10:49:29 +0100

    docker funcionando

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 # Configuración global de la API
-API_BASE = "https://dev.api.zoasuite.com/api"
-#API_BASE = "https://api.zoasuite.com/api"
-TOKEN= "sk_test_9f8a7b6c5d4e3f2a"
-#TOKEN = "prod_sk_7QfA9R2eWcLZp6mN4H8XxKJtD5yB0uS1VGaEOrU"
+# Whatsapp general
+#API_BASE = "https://dev.api.zoasuite.com/api"
+#TOKEN= "sk_test_9f8a7b6c5d4e3f2a"
+
+
+# VIMA
+API_BASE = "https://api.zoasuite.com/api"
+TOKEN = "prod_sk_7QfA9R2eWcLZp6mN4H8XxKJtD5yB0uS1VGaEOrU"
